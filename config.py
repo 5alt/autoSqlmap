d44bf2f2d742993ccf6c95aa5041831dbfd1fe14
WisD0m <salt@WisD0m-2.local>
2015-07-03 13:01:32 +0800

    small fix

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 import multiprocessing 
 
-save_path = '/tmp/tttt'
+save_path = '/tmp'
 
 proxy_host = '127.0.0.1'
 proxy_port = 8888
 
 filter_file = ['.css', '.js', '.jpg', '.jpeg', '.gif', '.png', '.bmp', '.html', '.htm', '.swf', '.svg'] #host.endswith(item)
 filter_code = r'4\d{2}' #re.match
-included_host = ['vulnweb.com'] #host.endswith(item)
+included_host = ['vulnweb.com'] #host.endswith(item), [''] for all
 excluded_host = ['google.com', '127.0.0.1'] #host.endswith(item)
 
 sqlmap_host = 'http://127.0.0.1:8775'
